a48b543ec5c422544c3397a43e8fa610c26bb15d
Jacob <jacob.peat01@gmail.com>
2022-04-16 08:11:48 +0100

    create function to fetch blog posts from api

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const blogTextRef = document.querySelector("#blog-text");
 const submitBtnRef = document.querySelector("#submit-btn");
 const formRef = document.querySelector("form");
 let recentBlogPostsRef = document.querySelector("#recent-blog-posts")
+
+async function getBlogPosts(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json()
+    console.log(data)
+}
+getBlogPosts()
