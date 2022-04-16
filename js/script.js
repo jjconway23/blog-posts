3d3b53cf01a56fb33cc7cf90e3f40e8add36abe2
Jacob <jacob.peat01@gmail.com>
2022-04-16 08:07:07 +0100

    Add div to store blog post and create reference to it in script.js aswell as the form element

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const blogTitleRef = document.querySelector("#blog-title");
 const blogTextRef = document.querySelector("#blog-text");
 const submitBtnRef = document.querySelector("#submit-btn");
-
+const formRef = document.querySelector("form");
+let recentBlogPostsRef = document.querySelector("#recent-blog-posts")
